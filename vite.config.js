4e1cba53a0ad8c29386c951a546a866f728407d5
vahidghaiem <vahidghaiem297@gmail.com>
2025-10-08 14:05:06 +0330

    fix: completely replace vite.config.js without vitest

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/vahidghaiem297/SheetMagic.git', // مهم! اسم ریپوی تو رو بذار
-   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./tests/setupTests.ts'],
-    css: true,
-    restoreMocks: true,
-  },
+  base: '/SheetMagic/',
+  build: {
+    outDir: 'dist'
+  }
 })
