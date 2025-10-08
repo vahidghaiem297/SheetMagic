be50da7fb6c974f2c00456a3f2be340cb9cace94
vahidghaiem <vahidghaiem297@gmail.com>
2025-10-08 18:50:34 +0330

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,25 +7,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
-    // اضافه کردن این تنظیمات
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-      },
-    },
+    sourcemap: false
   },
   server: {
     port: 5173,
     host: true
-  },
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg']
+  }
 })
