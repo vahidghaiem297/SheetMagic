2f14ee9d9bed359a6105f9aa212cc2144f7ef9fd
vahidghaiem <vahidghaiem297@gmail.com>
2025-10-08 17:46:37 +0330

    رهفث-زخدبهل

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SheetMagic/',
+  base: './', // این رو تغییر بدید
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
